8f2754db76ff4b39be625622bb9db1f35aa9e108
guilhermerogattotls <guilherme.r.ext@tlscontact.com>
2024-11-03 20:05:56 +0000

    adding the commandline globally

--- commandline.js
+++ commandline.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { pdfModelServer } from './src/pdf/pdf-model-server.js'
 import { testPlanCommandLine } from './src/command-line/test-plan-commandline.js'
 import { getPDFContentForServer } from './src/pdf/models/template.js'
