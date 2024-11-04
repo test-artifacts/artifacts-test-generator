95ee7b418a5315821f7fefffa95158b400354812
guilhermerogattotls <guilherme.r.ext@tlscontact.com>
2024-11-04 20:33:58 +0000

    adding new test plan structure

--- example-pdf-creation-server.js
+++ example-pdf-creation-server.js
@@ -1,5 +1,5 @@
 import { pdfModelServer } from './src/pdf/pdf-model-server.js'
 import { getPDFContentForServer } from './src/pdf/models/template.js'
 
-let pdfContent = getPDFContentForServer('My project', 'frontend', 'device')
+let pdfContent = getPDFContentForServer('Google', 'frontend', 'device')
 pdfModelServer(pdfContent)
